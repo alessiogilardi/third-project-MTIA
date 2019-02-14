89ddfc90b1e88f5303159b64751230c22e4a5360
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2019-02-14 14:48:16 +0100

    Update

--- FIorenzuola - DADM - Funzionante/test_DADM.m
+++ FIorenzuola - DADM - Funzionante/test_DADM.m
@@ -3,6 +3,7 @@ clc
 out_test    = [];
 predicted   = [];
 for i = 731:848
+    % Predispongo i dati per i restanti 118 giorni (Test)
     i1 = i - 10;
     in_test     = [X_gblu(i1:i1+9) X_gaso(i1:i1+9) X_benz(i1:i1+9) cal(i1:i1+9,3)' cal(i1+10,3)' cal(i1+11,3)']';
     out_test    = [out_test [X_gblu(i1+10) X_gaso(i1+10) X_benz(i1+10)]'];
@@ -12,10 +13,40 @@ end
 predicted(predicted < 0) = 0;
 
 error = out_test - predicted;
-%meanSquareError = diag(error*error')/size(out_test,2)
+
+startDate   = datenum('01-01-2018');
+endDate     = datenum('28-04-2018');
+
+xDate = linspace(startDate, endDate, size(out_test,2));
+plot(xDate, predicted(1,:), '-o')
+hold on
+plot(xDate, out_test(1,:), '-o')
+
 
 gblu_MSE = (error(1,:)*error(1,:)')/size(out_test,2);
 gaso_MSE = (error(2,:)*error(2,:)')/size(out_test,2);
 benz_MSE = (error(3,:)*error(3,:)')/size(out_test,2);
 
 MSE = [gblu_MSE, gaso_MSE, benz_MSE]
+
+
+% Conversione in valori binari
+predicted_bin = predicted;
+predicted_bin(predicted_bin >= 0.5) = 1;
+predicted_bin(predicted_bin < 0.5)  = 0;
+
+out_bin = out_test;
+out_bin(out_bin > 0) = 1;
+
+err_bin = out_bin - predicted_bin;
+gblu_MSE_bin = (err_bin(1,:)*err_bin(1,:)')/size(out_test,2);
+gaso_MSE_bin = (err_bin(2,:)*err_bin(2,:)')/size(out_test,2);
+benz_MSE_bin = (err_bin(3,:)*err_bin(3,:)')/size(out_test,2);
+
+MSE = [gblu_MSE_bin, gaso_MSE_bin, benz_MSE_bin]
+
+figure('Name', 'Binary Prediction')
+plot(xDate, predicted_bin(1,:), 'o')
+hold on
+plot(xDate, out_bin(1,:), 'o')
+
--- FIorenzuola - DADM - Funzionante/train_DADM.m
+++ FIorenzuola - DADM - Funzionante/train_DADM.m
@@ -2,18 +2,22 @@ clc;
 close all;
 clear;
 
-% Load data
+%% Load data
 a_2016 = csvread('2016.csv',1);
 a_2017 = csvread('2017.csv',1);
 a_2018 = csvread('2018.csv',1);
 
 a_2017 = a_2017(:,1:end-1);
 a_2018 = a_2018(:,1:end-1);
+
+% Carico il calendario con i pesi dei giorni (Sabato -> 0.5, Domenica -> 1)
 cal    = csvread('Calendar.csv',1);
+
+%% Genero il dataset con i tre anni
 X = [a_2016; a_2017; a_2018];
 clear a_2016 a_2017 a_2018
 
-%%
+%% Seleziono un punto vendita e estraggo i dati
 PVs = unique(X(:,2));
 pv_selez = 13846; % PV per cui voglio fare la predizione
 X(:,1) = X(:,1) - 42369; % Normalizzo la data
@@ -21,10 +25,14 @@ X_gblu((X(X(:,2)==pv_selez,1))) = X(X(:,2) == pv_selez,3);
 X_gaso((X(X(:,2)==pv_selez,1))) = X(X(:,2) == pv_selez,4);
 X_benz((X(X(:,2)==pv_selez,1))) = X(X(:,2) == pv_selez,5);
 
-%%
+%% Genero le matrici di input e output per il train della rete neurale (primi 730 giorni)
 in_mat  = [];
 out_mat = [];
 for i = 1:730
+    % Input e Output per 2 anni: 2016,2017
+    % 10 giorni per il GAS Blu, 10 GAS, 10 giorni per la Benzina, 10 giorni per
+    % il calendario e 2 giorni in più per passare a lunedì dal sabato e dalla
+    % domenica -> in totale mando in input 42 dati
     in_mat  = [in_mat [X_gblu(i:i+9) X_gaso(i:i+9) X_benz(i:i+9) cal(i:i+9,3)' cal(i+10,3)' cal(i+11,3)']'];
     out_mat = [out_mat [X_gblu(i+10) X_gaso(i+10) X_benz(i+10)]'];
 end
